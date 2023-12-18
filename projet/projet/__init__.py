d5ee1bd3f429ee9c6db2b80d6d3bb59020cf3ecd
mkonate19 <morikekonatekm12@gmail.com>
2023-12-18 16:56:20 +0100

    session

--- projet/projet/__init__.py
+++ projet/projet/__init__.py
@@ -1,11 +1,12 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-    with Configurator(settings=settings) as config:
+    my_session_factory = SignedCookieSessionFactory('itsasecret')
+    with Configurator(settings=settings, session_factory=my_session_factory) as config:
         config.include('pyramid_jinja2')
         config.add_static_view('static', 'static', cache_max_age=3600)
         config.include('.routes')
+        config.add_route('login', '/login')  # Ajout de la route pour la page de connexion
         config.scan()
     return config.make_wsgi_app()
--- projet/projet/views/views.py
+++ projet/projet/views/views.py
@@ -1,5 +1,9 @@
+import json
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
+
+# Définissez le chemin du fichier users.json dans le répertoire 'views'
+USERS_JSON_PATH = 'users.json'  # Mettez à jour le chemin du fichier
 
 @view_config(route_name='new_home', renderer='templates/new_home.jinja2')
 def new_home_view(request):
@@ -19,13 +23,54 @@ def register_view(request):
 
         # Ajoutez ici la logique pour valider le formulaire
         # Par exemple, vérifiez si le mot de passe et la confirmation sont identiques
-
         if password == confirm_password:
-            # Validation réussie, vous pouvez ajouter ici la logique pour stocker l'utilisateur, par exemple, dans une base de données
-            return HTTPFound(location=request.route_url('new_home'))  # Mettez à jour 'home' ici
+            # Charger les utilisateurs depuis le fichier JSON
+            try:
+                with open(USERS_JSON_PATH, 'r') as f:
+                    users = json.load(f)
+            except FileNotFoundError:
+                # Si le fichier n'existe pas, initialisez une structure vide
+                users = {}
+
+            # Vérifier si l'utilisateur existe déjà
+            if email in users:
+                return {'error_message': 'L\'utilisateur existe déjà'}
+
+            # Ajouter l'utilisateur à la liste
+            users[email] = {'password': password}
+
+            # Sauvegarder la liste mise à jour dans le fichier JSON
+            with open(USERS_JSON_PATH, 'w') as f:
+                json.dump(users, f)
+
+            # Validation réussie, rediriger vers la page de connexion
+            return HTTPFound(location=request.route_url('login'))  # Mettez à jour 'login' ici
         else:
-            # Validation échouée, vous pouvez renvoyer un message d'erreur à afficher dans le formulaire
+            # Validation échouée, renvoyer un message d'erreur à afficher dans le formulaire
             return {'error_message': 'Les mots de passe ne correspondent pas'}
 
     return {}
 
+@view_config(route_name='login', renderer='templates/login.jinja2')
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Charger les utilisateurs depuis le fichier JSON
+        try:
+            with open(USERS_JSON_PATH, 'r') as f:
+                users = json.load(f)
+        except FileNotFoundError:
+            # Si le fichier n'existe pas, initialisez une structure vide
+            users = {}
+
+        # Vérifier si l'utilisateur existe et si le mot de passe correspond
+        if email in users and users[email]['password'] == password:
+            # L'utilisateur est authentifié, rediriger vers la page d'accueil
+            return HTTPFound(location=request.route_url('new_home'))  # Mettez à jour 'new_home' ici
+        else:
+            # L'authentification a échoué, renvoyer un message d'erreur à afficher dans le formulaire
+            return {'error_message': 'L\'authentification a échoué'}
+
+    return {}
